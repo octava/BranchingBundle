956b4f286f58ab3a249f567f41fecb13e2a1c83c
Vladimir Pak <TheRatW@gmail.com>
2015-08-28 18:47:43 +0300

    fix config tree namespace

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('the_rat_branching');
+        $rootNode = $treeBuilder->root('octava_branching');
 
         /** @var ArrayNodeDefinition $rootNode */
         $rootNode
